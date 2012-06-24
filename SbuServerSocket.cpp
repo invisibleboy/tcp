1fc77934e4f3d275b2b90a242e04cf53a8fedfcf
MohammadKazemTaram <taram.mohammad@gmail.com>
2012-06-24 15:52:30 +0430

    thread of accept canceled.

--- SbuServerSocket.cpp
+++ SbuServerSocket.cpp
@@ -24,15 +24,10 @@ Segment* SbuServerSocket::SynackCreator(Segment *rcvd_seg)
 }
 SbuSocket SbuServerSocket::accept ()
 {
-    this->start();
-}
-void SbuServerSocket::run()
-{
     std::cout<<"wait for syn\n";
     while(1) {
         ip *iphdr;
         Segment* rcvd_segment= readFromRaw(iphdr);
-        printSegment(rcvd_segment);
         if(chkSum(rcvd_segment,false)!=0)
         {
             std::cout<<"***********Server************checksum faild "<<(int)chkSum(rcvd_segment,false)<<std::endl;
@@ -43,14 +38,32 @@ void SbuServerSocket::run()
         if(rcvd_segment->header.th_flags!=2)
             continue;
         std::cout<<">>>>>>>>>Server side:syn has been received from client:\n";
-//        printSegment(rcvd_segment);
         Segment* synack=SynackCreator(rcvd_segment);
-//        sleep(2);
         send(synack,false,inet_ntoa(iphdr->ip_src));
         std::cout<<">>>>>>>>>Server side: Synack has been sent\n";
-//        printSegment(synack);
+        //waiting for ack of synAck
+        Segment* rcvd_segment2= readFromRaw(iphdr);
+        if(chkSum(rcvd_segment2,false)!=0)
+        {
+            std::cout<<"***********Server************checksum faild "<<(int)chkSum(rcvd_segment,false)<<std::endl;
+            continue;
+        }
+        if(rcvd_segment2->header.th_dport!=myPort)
+            continue;
+        if(rcvd_segment2->header.th_flags!=16)
+            continue;
+        std::cout<<">>>>>>>>>Server side:ack of synAck has been received from client:\n";
+
+        state=ESTABILISHED;
+
         SbuSocket new_socket(inet_ntoa(iphdr->ip_src),(int)rcvd_segment->header.th_sport,this->myPort);
         connectedSockets+=new_socket;
+        return new_socket;
     }
+//    this->start();
+}
+void SbuServerSocket::run()
+{
+
     exec();
 }
